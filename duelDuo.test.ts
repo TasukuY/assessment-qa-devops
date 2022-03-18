0848bc72445531ce854fa6144f37167bc0c0c436
TasukuY <alohaty81@gmail.com>
2022-03-18 11:48:26 -0600

    added automated test

--- duelDuo.test.ts
+++ duelDuo.test.ts
@@ -1,4 +1,3 @@
-
 import { Builder, Capabilities, By } from "selenium-webdriver"
 
 require('chromedriver')
@@ -6,15 +5,36 @@ require('chromedriver')
 const driver = new Builder().withCapabilities(Capabilities.chrome()).build()
 
 beforeEach(async () => {
-    driver.get('http://localhost:3000/')
+    await driver.get('http://localhost:3000/')
+    driver.sleep(1000);
 })
 
 afterAll(async () => {
-    driver.quit()
+    await driver.quit()
 })
 
-test('Title shows up when page loads', async () => {
-    const title = await driver.findElement(By.id('title'))
-    const displayed = await title.isDisplayed()
-    expect(displayed).toBe(true)
+test('testing',async () => {
+    const seeAllbtn = await driver.findElement(By.id('see-all'));
+    expect(seeAllbtn.isDisplayed).toBeTruthy();
+    await driver.sleep(2000);
 })
+
+
+
+// test('Title shows up when page loads', async () => {
+//     const title = await driver.findElement(By.id('title'))
+//     const displayed = await title.isDisplayed()
+//     expect(displayed).toBe(true)
+// })
+
+// test("Check that clicking the Draw button displays the div with id = 'choices'", async () => {
+//     const title = await driver.findElement(By.id('title'))
+//     const displayed = await title.isDisplayed()
+//     expect(displayed).toBe(true)
+// })
+
+// test("Check that clicking an “Add to Duo” button displays the div with id = 'player-duo'", async () => {
+//     const title = await driver.findElement(By.id('title'))
+//     const displayed = await title.isDisplayed()
+//     expect(displayed).toBe(true)
+// })
